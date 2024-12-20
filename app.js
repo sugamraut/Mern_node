6e68a6f5b8be0a134e3f0c61ec8fd7da9a5584b4
Sugam Raut <sugamrautbim@gmail.com>
2024-12-20 22:45:44 +0545

    include the get req message

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express= require('express');
 const app=express();
+port=3000;
 
-app.listen(3000)
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Hello World!!"
+    })
+})
+app.get("/about",(req,res)=>{
+    res.send("about")
+})
+app.listen(`${port}`,()=>{
+    console.log(`this app is start at port nunmber ${port}`)
+})
