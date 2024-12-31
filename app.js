9d33bd833932d5a198cd1b4e00d8456e3593c9f4
Sugam Raut <sugamrautbim@gmail.com>
2024-12-31 22:03:22 +0545

    ORM and ODM

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app=express();
 port=3000;
 
 app.get("/",(req,res)=>{
-    res.json({
+    res.status(200).json({
         message:"Hello World!!"
     })
 })
 app.get("/about",(req,res)=>{
-    res.send("about")
+    res.status(200).json({
+        message:"hello i am about page!!"
+    })
 })
 app.listen(`${port}`,()=>{
     console.log(`this app is start at port nunmber ${port}`)
